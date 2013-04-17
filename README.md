d900b11ccd6ae679ef3c278a67a3dcff22bfff7f
Tim C <foamyguy@gmail.com>
2013-04-17 08:46:07 -0500

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 GoogleFormUploadExample
 =======================
 
+If you are just interested in integrating with forms, and don't care too much about the details of how it works I suggest using the [GoogleFormUploader] [1] Library that I have created. This project will give you the "behind the scenes look" at how it works.
+
 Example Android application that demonstrates how to upload some data into a Google form/spreadsheet.
+
+[1]: https://github.com/FoamyGuy/GoogleFormUploader     "GoogleFormUploader"
--- src/com/makemyandroidapp/example/googlespreadsheet/post/MainActivity.java
+++ src/com/makemyandroidapp/example/googlespreadsheet/post/MainActivity.java
@@ -1,21 +1,6 @@
 package com.makemyandroidapp.example.googlespreadsheet.post;
 
-import java.io.IOException;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -25,7 +10,7 @@ public class MainActivity extends Activity {
 	
 	final String myTag = "DocsUpload";
 	
-
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +28,16 @@ public class MainActivity extends Activity {
 	}
 
 	
+
 	public void postData() {
 
-		String fullUrl = "https://docs.google.com/forms/d/15eWiE2LhJWlyQGb437Ol82wNKmfK1-0vaOgktTuEwyk/formResponse";
+		String fullUrl = "https://docs.google.com/forms/d/1AYvV0gFgB1hBuoRKnMsXy1LyF8-Ce8VAshAtho6Z08s/formResponse";
 		HttpRequest mReq = new HttpRequest();
 		String col1 = "Hello";
 		String col2 = "World";
-		String data = "entry_451758497=" + URLEncoder.encode(col1) + "&" + 
-					  "entry_1932654660=" + URLEncoder.encode(col2);
+		
+		String data = "entry.1680144410=" + URLEncoder.encode(col1) + "&" + 
+					  "entry.1558298396=" + URLEncoder.encode(col2);
 		String response = mReq.sendPost(fullUrl, data);
 		Log.i(myTag, response);
 	} 
